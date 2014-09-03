eeec4248a8ddd805adf86d92007eb8d1547af91c
Masato Ikeda <masato.ikeda@gmail.com>
2014-09-03 19:10:50 +0900

    Add Enumerable#natsort_by

--- lib/natsort.rb
+++ lib/natsort.rb
@@ -5,4 +5,8 @@ module Enumerable
   def natsort
     sort { |a, b| Natcmp.natcmp(a.to_s, b.to_s) }
   end
+
+  def natsort_by(&block)
+    sort { |a, b| Natcmp.natcmp(block.call(a).to_s, block.call(b).to_s) }
+  end
 end
--- test/natsort_test.rb
+++ test/natsort_test.rb
@@ -10,4 +10,20 @@ class NatsortTest < MiniTest::Test
     actual = [1, 10, 2].natsort
     assert_equal [1, 2, 10], actual
   end
+
+  def test_natsort_by
+    actual = [
+      { "m" => "1", "n" => "1" },
+      { "m" => "2", "n" => "10" },
+      { "m" => "3", "n" => "2" }
+    ].natsort_by { |x| x["n"] }
+
+    expected = [
+      { "m" => "1", "n" => "1" },
+      { "m" => "3", "n" => "2" },
+      { "m" => "2", "n" => "10" }
+    ]
+
+    assert_equal expected, actual
+  end
 end
