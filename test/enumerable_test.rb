b301873d69f7f3dd8e8b2df2bd198758ac320f7c
Masato Ikeda <masato.ikeda@gmail.com>
2014-09-03 19:13:59 +0900

    Rename test files

--- test/enumerable_test.rb
+++ test/enumerable_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class NatsortTest < MiniTest::Test
+class EnumerableTest < MiniTest::Test
   def test_natsort_strings
     actual = ["1", "10", "2"].natsort
     assert_equal ["1", "2", "10"], actual
